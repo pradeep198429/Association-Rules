61bce118eb940220aeea639764903bf18804af11
pradeep198429 <68952458+pradeep198429@users.noreply.github.com>
2020-12-10 01:05:51 +1100

    Update Association_Rules_Book.py

--- Association_Rules_Book.py
+++ Association_Rules_Book.py
@@ -1,18 +1,52 @@
 import pandas as pd
+#it is used for apriori algorithm 
+from mlxtend.frequent_patterns import apriori,association_rules
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+
 
 X=pd.read_csv("E:\\Data Science\\Assignments\\Python code\\Association_Rules\\book.csv")
 
-from mlxtend.frequent_patterns import apriori,association_rules
-frequent_items=apriori(X, min_support=0.005, max_len=3,use_colnames = True)
+frequent_itemsets = apriori(X, min_support=0.0035, max_len=4,use_colnames = True)
+frequent_itemsets.sort_values('support',ascending = False,inplace=True)
+figure(num=None, figsize=(30rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1), 10), dpi=80, facecolor='w', edgecolor='k')
+plt.bar(x = list(range(1,11)),height = frequent_itemsets.support[1:11],color='rgmyk');plt.xticks(list(range(1,11)),frequent_itemsets.itemsets[1:11])
+plt.xlabel('item-sets',fontsize = 20);plt.ylabel('support', fontsize=20);plt.title('top 10 list sets with minsupport=0.0035 and max_length=4', fontsize=40)
 
-frequent_items.sort_values('support',ascending=False,inplace=True)
 
-rules=association_rules(frequent_items,metric='lift', min_threshold=1)
+rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+rules.shape
 
-print(rules.sort_values('lift',ascending=False).head())
+rules.sort_values('lift',ascending = False,inplace=True)
+rules.head(20)
 
-import matplotlib.pyplot as plt
+#min_support=0.0035 , max_len=3
 
-plt.bar(x = list(range(1,11)),height = frequent_items.support[1:11],color='rgmyk');
-plt.xticks(list(range(1,11)),frequent_items.itemsets[1:11])
-plt.xlabel('item-sets');plt.ylabel('support')
+frequent_itemsets = apriori(X, min_support=0.0035, max_len=3,use_colnames = True)
+
+frequent_itemsets.sort_vafrequent_itemsets.sort_values('support',ascending = False,inplace=True)lues('support',ascending = False,inplace=True)
+
+figure(num=None, figsize=(30, 10), dpi=80, facecolor='w', edgecolor='k')
+plt.bar(x = list(range(1,11)),height = frequent_itemsets.support[1:11],color='rgmyk');plt.xticks(list(range(1,11)),frequent_itemsets.itemsets[1:11])
+plt.xlabel('item-sets',fontsize = 20);plt.ylabel('support', fontsize=20);plt.title('top 10 list sets with minsupport=0.0035 and max_length=3', fontsize=40)
+
+
+rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+rules.sort_values('lift',ascending = False,inplace=True)
+rules.head(20)
+
+#min_support=0.007 , max_len=
+
+frequent_itemsets = apriori(X, min_support=0.0035, max_len=4,use_colnames = True)
+frequent_itemsets.sort_values('support',ascending = False,inplace=True)
+figure(num=None, figsize=(30rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1), 10), dpi=80, facecolor='w', edgecolor='k')
+plt.bar(x = list(range(1,11)),height = frequent_itemsets.support[1:11],color='rgmyk');plt.xticks(list(range(1,11)),frequent_itemsets.itemsets[1:11])
+plt.xlabel('item-sets',fontsize = 20);plt.ylabel('support', fontsize=20);plt.title('top 10 list sets with minsupport=0.0035 and max_length=4', fontsize=40)
+
+
+rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+rules.shape
+
+rules.sort_values('lift',ascending = False,inplace=True)
+rules.head(20)
